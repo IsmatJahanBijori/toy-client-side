8f7e1a636e4448f062b90e80343e611230d90d73
IsmatJahanBijori <ismatjahanbijori@gmail.com>
2023-05-17 23:31:05 +0600

    footer added

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,10 +1,64 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 const Footer = () => {
     return (
         <div>
-        Footer
+            <footer className="footer p-10 bg-slate-200 text-base-content rounded-t-lg">
+                <div>
+                    <img src="https://i.ibb.co/ctkd2qN/art.png" className='h-20' alt="" />
+                    <p>Toy Joy Online Ltd.<br />Providing reliable tech since 2023</p>
+                </div>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact: </a>
+                    <li>Email: toyjoy@gmail.com</li>
+                    <li>Phone no: 01911108125</li>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </div>
+                <div>
+                    <span className="footer-title">Social</span>
+                    <div className="flex flex-row justify-center p-4">
+                        <Link href='' className='me-4 text-reset '>
+                            <FaFacebook style={{ color: '#3B5998' }} />
+                        </Link>
+                        <Link href='' className='me-4 text-reset'>
+                            <FaTwitter style={{ color: '#00ACEE' }} />
+                        </Link>
+                        <Link href='' className='me-4 text-reset'>
+                            <FaInstagram style={{ color: 'red' }} />
+                        </Link>
+                        <Link href='' className='me-4 text-reset'>
+                            <FaGoogle style={{ color: '#DB4437' }}></FaGoogle>
+                        </Link>
+                        <Link href='' className='me-4 text-reset'>
+                            <FaLinkedin style={{ color: '#0072B1' }} />
+                        </Link>
+                        <Link href='' className='me-4 text-reset'>
+                            <FaGithub style={{ color: '#1a1110' }} />
+                        </Link>
+                    </div>
+                </div>
+            </footer>
+            <footer className="footer footer-center p-4 bg-slate-200 text-base-content pb-10 rounded-b-lg">
+                <div>
+                    <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+                </div>
+            </footer>
         </div>
     );
 };
