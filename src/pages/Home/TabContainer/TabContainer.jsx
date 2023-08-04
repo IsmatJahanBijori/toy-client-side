96191c27153c7f52c19d6df9134616e8870f9736
IsmatJahanBijori <ismatjahanbijori@gmail.com>
2023-08-04 23:25:23 +0600

    tab section added

--- src/pages/Home/TabContainer/TabContainer.jsx
+++ src/pages/Home/TabContainer/TabContainer.jsx
@@ -1,27 +1,103 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
 const TabContainer = () => {
+
+    //jewellary
+    const [data1, setData1] = useState([])
+    useEffect(() => {
+        fetch('jewelryMaking.json').then(res => res.json()).then(data => setData1(data))
+    }, []);
+
+    //drawing
+    const [data2, setData2] = useState([])
+    useEffect(() => {
+        fetch('drawingSupplies.json').then(res => res.json()).then(data => setData2(data))
+    }, []);
+
+    //sculpting
+    const [data3, setData3] = useState([])
+    useEffect(() => {
+        fetch('sculpting.json').then(res => res.json()).then(data => setData3(data))
+    }, []);
+
+
+    const [isHovered, setIsHovered] = useState(false);
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
     return (
         <div>
-            <Tabs className="m-28">
-                <TabList>
+            <Tabs className="mb-20 md:m-28 font-serif">
+                <TabList className="text-2xl font-semibold mb-3">
                     <Tab>Jewellary Making</Tab>
                     <Tab>Drawing Supplies</Tab>
                     <Tab>Sculpting</Tab>
                 </TabList>
+                {/*jewellary */}
+                <TabPanel>
+                    <div className='grid grid-cols-1 md:grid-cols-4'>
+                        {
+                            data1.map(dt1 =>
 
-                <TabPanel>
-                    <h2>Any content 1</h2>
-                    <h2>Any content 2</h2>
+                                <div className="bg-slate-100 ml-10 mb-5 w-[300px] md:w-[400px] shadow-md rounded-lg p-4 transition-transform duration-300 transform hover:translate-y-[-10px] text-xl" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                                    <img src={dt1.picture} alt="Card Image" className="w-full h-40 object-cover rounded-md mb-4" />
+                                    <div className={`transition-opacity duration-300 ${isHovered ? 'opacity-100' : 'opacity-0'} mb-2`}>
+                                        <h3 className="text-xl font-bold mb-2">{dt1.name}</h3>
+                                        <p className="text-gray-600 mb-2">${dt1.price}</p>
+                                        <p className="text-gray-800 mb-2">Ratings: {dt1.rating}</p>
+                                        <button className="bg-blue-500 text-white px-4 py-2 rounded-lg">View Details</button>
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </div>
                 </TabPanel>
+
+                {/*drawing */}
                 <TabPanel>
-                    <h2>Any content 3</h2>
-                    <h2>Any content 4</h2>
+                    <div className='grid grid-cols-1 md:grid-cols-4'>
+                        {
+                            data2.map(dt2 =>
+
+                                <div className="bg-slate-100 ml-10 mb-5 w-[300px] md:w-[400px] shadow-md rounded-lg p-4 transition-transform duration-300 transform hover:translate-y-[-10px] text-xl" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                                    <img src={dt2.picture} alt="Card Image" className="w-full h-40 object-cover rounded-md mb-4" />
+                                    <div className={`transition-opacity duration-300 ${isHovered ? 'opacity-100' : 'opacity-0'} mb-2`}>
+                                        <h3 className="text-xl font-bold mb-2">{dt2.name}</h3>
+                                        <p className="text-gray-600 mb-2">${dt2.price}</p>
+                                        <p className="text-gray-800 mb-2">Ratings: {dt2.rating}</p>
+                                        <button className="bg-blue-500 text-white px-4 py-2 rounded-lg">View Details</button>
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </div>
                 </TabPanel>
+
+                {/*sculpting */}
                 <TabPanel>
-                    <h2>Any content 5</h2>
-                    <h2>Any content 6</h2>
+                    <div className='grid grid-cols-1 md:grid-cols-4'>
+                        {
+                            data3.map(dt3 =>
+
+                                <div className="bg-slate-100 ml-10 mb-5 w-[300px] md:w-[400px] shadow-md rounded-lg p-4 transition-transform duration-300 transform hover:translate-y-[-10px] text-xl" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                                    <img src={dt3.picture} alt="Card Image" className="w-full h-40 object-cover rounded-md mb-4" />
+                                    <div className={`transition-opacity duration-300 ${isHovered ? 'opacity-100' : 'opacity-0'} mb-2`}>
+                                        <h3 className="text-xl font-bold mb-2">{dt3.name}</h3>
+                                        <p className="text-gray-600 mb-2">${dt3.price}</p>
+                                        <p className="text-gray-800 mb-2">Ratings: {dt3.rating}</p>
+                                        <button className="bg-blue-500 text-white px-4 py-2 rounded-lg">View Details</button>
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </div>
                 </TabPanel>
             </Tabs>
         </div>
