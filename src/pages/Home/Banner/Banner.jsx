e2f57a39403d9a6fd0f51d89c87a2f3a7ea6e9b8
IsmatJahanBijori <ismatjahanbijori@gmail.com>
2023-08-05 02:00:41 +0600

    banner small device edited

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -8,12 +8,7 @@ import { Navigation, Autoplay, Pagination, } from 'swiper/modules';
 
 const Banner = () => {
     const [banner, setBanner] = useState([])
-    // const progressCircle = useRef(null);
-    // const progressContent = useRef(null);
-    // const onAutoplayTimeLeft = (s, time, progress) => {
-    //     // progressCircle.current.style.setProperty('--progress', 1 - progress);
-    //     // progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
-    // };
+    
     useEffect(() => {
         fetch("banner.json").then(res => res.json()).then(data => setBanner(data))
     }, [])
@@ -39,13 +34,13 @@ const Banner = () => {
             {
                 banner.map(bannerData =>
                     <SwiperSlide>
-                        <div className="hero mb-20">
+                        <div className="hero mb-20 w-full">
                             <img src={bannerData.image} className='w-full h-[600px]' alt="" />
                             <div className="hero-content text-center text-neutral-content">
                                 <div className="hero-overlay pt-20">
-                                    <div className="w-[1440px] h-[400px]">
-                                        <h1 className="mb-5 text-5xl font-bold">{bannerData.title}</h1>
-                                        <p className="mb-5 text-3xl font-semibold">{bannerData.subtitle}</p>
+                                    <div className="w-[300px] md:w-[1440px] md:h-[400px]">
+                                        <h1 className="mb-5 tex-3xl md:text-5xl font-bold">{bannerData.title}</h1>
+                                        <p className="mb-5 text-lg font-semibold">{bannerData.subtitle}</p>
                                     </div>
                                 </div>
                             </div>
